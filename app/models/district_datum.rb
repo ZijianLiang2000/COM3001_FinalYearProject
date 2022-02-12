ed359613fb3dc8d234058f636bf9d875f7da7343
Brian Liang <brianbrain2010@hotmail.com>
2022-02-12 14:56:45 +0000

    Changed count index

--- app/models/district_datum.rb
+++ app/models/district_datum.rb
@@ -18,4 +18,11 @@ class DistrictDatum < ApplicationRecord
     end
     validates :name, presence: true, inclusion: { in: ladNames, message: "%{value} is not a valid Local Authority District Name" }
     validates :code, presence: true, inclusion: { in: ladCodes, message: "%{value} is not a valid Local Authority District Code" }
+
+    # All restaurant categories
+    Restaurant_Category_Encode = ["Italian Restaurant","Indian Restaurant","Japanese Restaurant","Thai Restaurant","British Restaurant","Chinese Restaurant","Vegetarian","Cafe","Pub"]
+    districtName = self.name
+    # for category in Restaurant_Category_Encode
+    #     DistrictDatum.where(name: districtName).restaurantData
+
 end
--- app/models/restaurant_datum.rb
+++ app/models/restaurant_datum.rb
@@ -1,3 +1,6 @@
 class RestaurantDatum < ApplicationRecord
     belongs_to :DistrictDatum
+
+    Restaurant_Category_Encode = ["Italian Restaurant","Indian Restaurant","Japanese Restaurant","Thai Restaurant","British Restaurant","Chinese Restaurant","Vegetarian","Cafe","Pub"]
+    
 end
